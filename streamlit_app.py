fb1ef87a96f2e86ae8731aabbb4e3f42a765609e
alexmathew98 <alexmathewdev2099@gmail.com>
2024-08-15 19:17:37 -0400

    Pushing streamlit file

---
+++ streamlit_app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from main import ResearchCrew  # Import the ResearchCrew class from main.py
+import os
+
+st.title('Your Research Assistant')
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+os.environ["SERPER_API_KEY"] = st.secrets["SERPER_API_KEY"]
+
+with st.sidebar:
+    st.header('Enter Research Details')
+    topic = st.text_input("Which competitor do you want to analyse:")
+    detailed_questions = st.text_area("Specific questions or subtopics you are interested in exploring:")
+
+if st.button('Run Research'):
+    if not topic or not detailed_questions:
+        st.error("Please fill all the fields.")
+    else:
+        inputs = f"Competition Company: {topic}\n"
+        research_crew = ResearchCrew(inputs)
+        result = research_crew.run()
+        st.subheader("Results of your research project:")
+        st.write(result)
